36fa8b26884b199280791917efc39d4c4de0e859
ctrachte <cetrachte@hotmail.com>
2017-10-30 09:07:02 -0500

    Add line for function found in Python documentation, per IPND instructions

--- profanity editor/check_profanity.py
+++ profanity editor/check_profanity.py
@@ -1,6 +1,8 @@
 def read_text():
 	quotes = open("C:\Users\Caleb\Desktop\UDACITY\Python\Misc\profanity editor\profanity_test.txt")
 	contents = quotes.read()
+	number_of_words = len(contents)
+	print("Number of characters in file = " + str(number_of_words))
 	print(contents)
 
 read_text()
