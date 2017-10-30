bc555e370ee0f69c795971272515575b0fce2586
ctrachte <cetrachte@hotmail.com>
2017-10-30 09:18:10 -0500

    Add function to check text for profanity

--- profanity editor/check_profanity.py
+++ profanity editor/check_profanity.py
@@ -1,8 +1,17 @@
+import urllib
+
 def read_text():
 	quotes = open("C:\Users\Caleb\Desktop\UDACITY\Python\Misc\profanity editor\profanity_test.txt")
 	contents = quotes.read()
 	number_of_words = len(contents)
 	print("Number of characters in file = " + str(number_of_words))
 	print(contents)
+	quotes.close()
+	check_profanity(contents)
 
+def check_profanity(text_to_check):
+	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+	output = connection.read()
+	print(output)
+	connection.close()
 read_text()
