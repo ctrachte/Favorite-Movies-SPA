b4c42904ab15f6080f3ab3d8f47d420a5f812066
ctrachte <cetrachte@hotmail.com>
2017-10-31 10:44:12 -0500

    Add movie instance variable for "Oldboy"

--- movie_website/entertainment_center.py
+++ movie_website/entertainment_center.py
@@ -42,7 +42,9 @@ prometheus = media.Movie(movies['movie4']['title'], movies['movie4']['plot_summa
 
 hot_rod = media.Movie(movies['movie5']['title'], movies['movie5']['plot_summary'],movies['movie5']['poster_image'],movies['movie5']['trailer'])
 
-movie_ids = [blade_runner,gits_1995,blade_runner_2049,prometheus,hot_rod]
+oldboy = media.Movie(movies['movie6']['title'], movies['movie6']['plot_summary'],movies['movie6']['poster_image'],movies['movie6']['trailer'])
+
+movie_ids = [blade_runner,gits_1995,blade_runner_2049,prometheus,hot_rod, oldboy]
 
 fresh_tomatoes.open_movies_page(movie_ids)
 
