cc1c99744101ea7576d65f40c361dc46e250d359
ctrachte <cetrachte@hotmail.com>
2017-11-01 18:12:49 -0500

    Correct syntax error defining variables

--- movie_website/fresh_tomatoes.py
+++ movie_website/fresh_tomatoes.py
@@ -195,11 +195,11 @@ def create_movie_tiles_content(movies):
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id,
-            movie_rating=rating,
-            movie_genre=genre,
-            movie_director=director,
-            movie_duration=duration,
-            movie_year=year
+            movie_rating=movie.rating,
+            movie_genre=movie.genre,
+            movie_director=movie.director,
+            movie_duration=movie.duration,
+            movie_year=movie.year
         )
     return content
 
